160f668d48e63379777bc87fddbec57338ae0ff3
suskull <xoaixanh.muoii95@gmail.com>
2020-06-26 00:52:55 +0700

    Updates

---
+++ precache-manifest.5ee2329c89637c17ae96303fdb8a2b89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee92e3eccb311f7bdda68613c79d49c",
+    "url": "/KeeperApp/index.html"
+  },
+  {
+    "revision": "384be872998ab7e1f41b",
+    "url": "/KeeperApp/static/js/2.5a745460.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/KeeperApp/static/js/2.5a745460.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "641565f4820a4b7e23d1",
+    "url": "/KeeperApp/static/js/main.91106b6e.chunk.js"
+  },
+  {
+    "revision": "629b862f45c1ea3802e7",
+    "url": "/KeeperApp/static/js/runtime-main.c5a09080.js"
+  }
+]);
